7ea5f7e4448f4c2150417738468c895aa61d8d85
sunilkumar1996 <sunilrajputdev@gmail.com>
2020-12-02 22:40:23 +0530

    rest framework function based view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework.renderers import JSONRenderer
+from django.http import HttpResponse, JsonResponse
 
-# Create your views here.
+def student_detail(request):
+    student = Student.objects.get(pk=1)
+    serializer = StudentSerializer(student)
+    # json_data = JSONRenderer().render(serializer.data)
+    # return HttpResponse(json_data, content_type='application/json')
+    return JsonResponse(serializer.data)
+
+def student_list(request):
+    student = Student.objects.all()
+    serializer = StudentSerializer(student, many=True)
+    json_data = JSONRenderer().render(serializer.data)
+    return HttpResponse(json_data, content_type="application/json")
